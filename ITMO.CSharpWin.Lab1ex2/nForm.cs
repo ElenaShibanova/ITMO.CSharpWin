12887ecd92b48e37b0ab5ccccbefa9eb7abdf117
Elena Shibanova <esshibanova1985@gmail.com>
2022-01-14 22:55:47 +0300

    Лабораторная 1

---
+++ ITMO.CSharpWin.Lab1ex2/nForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CSharpWin.Lab1ex2
+{
+    public partial class nForm : ITMO.CSharpWin.Lab1ex2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITMO.CSharpWin.Lab1ex5Task1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ITMO.CSharpWin.Lab1ex5Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCloseForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonCloseForm
+            // 
+            this.ButtonCloseForm.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ButtonCloseForm.Location = new System.Drawing.Point(200, 150);
+            this.ButtonCloseForm.Name = "ButtonCloseForm";
+            this.ButtonCloseForm.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCloseForm.TabIndex = 0;
+            this.ButtonCloseForm.Text = "Close Form";
+            this.ButtonCloseForm.UseVisualStyleBackColor = false;
+            this.ButtonCloseForm.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.ButtonCloseForm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCloseForm;
+    }
+}
+
